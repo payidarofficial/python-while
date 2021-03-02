05c1eaebbe99b4b5838b8cc0156eb5eebe2350cf
Pâyidar <79402324+payidarofficial@users.noreply.github.com>
2021-03-02 20:09:33 +0300

    Update and rename payidar.js to payidar.py

---
+++ payidar.py
@@ -0,0 +1,14 @@
+# ##################################################################################
+# Selam Dostlarım Ben Pâyidar...                                                   #
+# Öncelikle Sizlere Python Programlama Dilinde "While" Döngüsünü Anlatacağım...    #
+# ##################################################################################
+
+# Öncelikle Pâyidar İsminde Bir Değişken Atayalım Ve Bu '1' Olsun.
+payidar = 1
+
+# 1 Sayısı 30 Olana Kadar Ekrana Sayı Yazdır Dedik.
+while payidar < 30:
+  print(payidar)
+  payidar =+ 30
+  
+# += Bu İşaret Artı Eşittir Oluyor. Yani 1 Olan Yazımız 30'a Eşit Olana Kadar Durma. Umarım Anlamışsınızdır...
